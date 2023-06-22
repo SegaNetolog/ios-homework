0be387a0b0998a18a848661df18c3a1a19db6a70
Sergey Bragin <sergeybragin@MacBook-Air-Evgeniy-2.local>
2023-06-22 13:30:17 +0300

    task 7

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -8,6 +8,45 @@
 import UIKit
 class ProfileHeaderView: UIView {
     
+    private var leadingImageView = NSLayoutConstraint()
+    private var trailingImageView = NSLayoutConstraint()
+    private var topImageView = NSLayoutConstraint()
+    private var widthImageView = NSLayoutConstraint()
+    private var heightImageView = NSLayoutConstraint()
+    private var heightWrapView = NSLayoutConstraint()
+    
+    private var statusLabel: String = ""
+    private var setStatusButton: UIButton!
+    private let avatarLayView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let wrapView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        view.layer.opacity = 0.5
+        view.isHidden = true
+        return view
+    }()
+    
+    
+    private lazy var closeBigBin: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.image =  UIImage(systemName: "xmark.circle")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isUserInteractionEnabled = true
+        imageView.layer.opacity = 0
+        imageView.tintColor = .white
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapCloseGesture)))
+        return imageView
+    }()
+    
     private var statusText: String = " "
 
     private let titleName: UILabel = {
@@ -33,16 +72,20 @@ class ProfileHeaderView: UIView {
 
         return button
     }()
-    private let imageView: UIImageView = {
+    
+    
+    private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.layer.borderColor = UIColor.white.cgColor
         imageView.layer.borderWidth = 3
-        imageView.layer.cornerRadius = 60
+        imageView.layer.cornerRadius = 50
         imageView.image = UIImage(named: "mister")
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isUserInteractionEnabled = true
 
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(openBigBin)))
         return imageView
     }()
     
@@ -89,35 +132,109 @@ class ProfileHeaderView: UIView {
     }
         
     func subview() {
-        addSubview(imageView)
         addSubview(titleName)
         addSubview(titleStatus)
         addSubview(button)
         addSubview(textField)
+        addSubview(avatarLayView)
+        addSubview(wrapView)
+        addSubview(imageView)
+        addSubview(closeBigBin)
     }
     
     func constrains() {
+        
+        
+        leadingImageView = imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16)
+        topImageView = imageView.topAnchor.constraint(equalTo: topAnchor,constant: 16)
+        widthImageView = imageView.widthAnchor.constraint(equalToConstant: 95)
+        heightImageView = imageView.heightAnchor.constraint(equalToConstant: 95)
+        NSLayoutConstraint.activate([leadingImageView, topImageView, widthImageView, heightImageView])
+        
+        heightWrapView = wrapView.heightAnchor.constraint(equalToConstant: 0)
+        NSLayoutConstraint.activate([heightWrapView])
+        
         NSLayoutConstraint.activate([
+            
+            wrapView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            wrapView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            wrapView.topAnchor.constraint(equalTo: topAnchor),
+            
+            closeBigBin.widthAnchor.constraint(equalToConstant: 30),
+            closeBigBin.heightAnchor.constraint(equalToConstant: 30),
+            closeBigBin.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            closeBigBin.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+            
+            avatarLayView.widthAnchor.constraint(equalToConstant: 95),
+            avatarLayView.heightAnchor.constraint(equalToConstant: 95),
+            avatarLayView.topAnchor.constraint(equalTo: topAnchor,constant: 16),
+            avatarLayView.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 16),
+            
             button.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 15),
             button.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             button.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
             button.heightAnchor.constraint(equalToConstant: 50),
             button.bottomAnchor.constraint(equalTo: bottomAnchor,constant:  -25),
-            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
-            imageView.widthAnchor.constraint(equalToConstant: 120),
-            imageView.heightAnchor.constraint(equalToConstant: 120),
+           
             titleStatus.leadingAnchor.constraint(equalTo: titleName.leadingAnchor),
             titleStatus.topAnchor.constraint(equalTo: titleName.bottomAnchor, constant: 15),
             titleStatus.widthAnchor.constraint(equalToConstant: 170),
-            titleName.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 10),
+          
+            titleName.leadingAnchor.constraint(equalTo: avatarLayView.trailingAnchor, constant: 10),
             titleName.topAnchor.constraint(equalTo: topAnchor, constant: 30),
             titleName.widthAnchor.constraint(equalToConstant: 190),
+        
             textField.topAnchor.constraint(equalTo: titleStatus.bottomAnchor, constant: 12),
             textField.heightAnchor.constraint(equalToConstant: 50),
             textField.widthAnchor.constraint(equalToConstant: 210),
             textField.leadingAnchor.constraint(equalTo: titleName.leadingAnchor),
             textField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
         ])
+    }
+    @objc private func openBigBin() {
+        UIView.animateKeyframes(withDuration: 0.8, delay: 0) {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.6) {
+                
+                self.widthImageView.constant = UIScreen.main.bounds.width
+                self.leadingImageView.constant = 0
+                self.trailingImageView.constant = 0
+                self.topImageView.constant = 100
+                self.heightImageView.constant = self.widthImageView.constant
+                self.heightWrapView.constant =  UIScreen.main.bounds.height
+                self.imageView.layer.cornerRadius = 0
+//                self.imageView.centerYAnchor.constraint(equalTo: self.wrapView.centerYAnchor)
+                self.wrapView.isHidden = false
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.6, relativeDuration: 0.8) {
+                self.closeBigBin.layer.opacity = 1
+                self.wrapView.layer.opacity = 0.3
+
+            }
+        }
+        
+    }
+    
+    @objc private func tapCloseGesture() {
+        
+        UIView.animateKeyframes(withDuration: 0.4, delay: 0) {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.1) {
+                self.closeBigBin.layer.opacity = 0
+                self.wrapView.layer.opacity = 0
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.1) {
+                self.wrapView.isHidden = false
+                self.wrapView.isHidden = true
+                self.imageView.layer.cornerRadius = 50
+                self.widthImageView.constant = 95
+                self.leadingImageView.constant = 16
+                self.trailingImageView.constant = 30
+                self.topImageView.constant = 16
+                self.heightImageView.constant = 95
+                self.heightWrapView.constant =  UIScreen.main.bounds.height
+            }
+        }
     }
 }
